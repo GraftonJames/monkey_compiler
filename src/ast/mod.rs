8bb768feaa0a5cb287442c652852401b16d45177
james grafton <james.grafton9@gmail.com>
2024-05-15 17:23:44 +0100

    refactor to tuple type node wrappers

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,118 +1,87 @@
 use super::token::Token;
 
 pub trait Node {
-        fn token_literal(&self) -> String;
-        fn string(&self) -> String;
-}
-
-pub struct Statement(pub Box<dyn Node>);
-impl Node for Statement {
-        fn token_literal(&self) -> String {
-                self.0.token_literal()
-        }
-
-        fn string(&self) -> String {
-                self.0.string()
-        }
-}
-
-pub struct Expression(pub Box<dyn Node>);
-impl Node for Expression {
-        fn token_literal(&self) -> String {
-                self.0.token_literal()
-        }
-
-        fn string(&self) -> String {
-                self.0.string()
-        }
+    fn token_literal(&self) -> String;
+    fn string(&self) -> String;
 }
 
 pub struct Program {
-        pub statements: Vec<Statement>,
+    pub statements: Vec<BoxNode>,
 }
-impl Node for Program {
-        fn token_literal(&self) -> String {
-                if self.statements.len() > 0 {
-                        self.statements[0].token_literal()
-                } else {
-                        "".to_string()
-                }
-        }
 
-        fn string(&self) -> String {
-                self.statements
-                        .iter()
-                        .clone()
-                        .fold("".to_string(), |acc, x| acc + "/n" + &x.string())
+type BoxNode = Box<dyn Node>;
+impl Node for Program {
+    fn token_literal(&self) -> String {
+        if self.statements.len() > 0 {
+            self.statements[0].token_literal()
+        } else {
+            "".to_string()
         }
+    }
+
+    fn string(&self) -> String {
+        self.statements
+            .iter()
+            .clone()
+            .fold("".to_string(), |acc, x| acc + "/n" + &x.string())
+    }
 }
 
 pub struct ExpressionStatement {
-        pub token: Token,
-        pub expression: Expression,
+    pub token: Token,
+    pub expression: BoxNode,
 }
 impl Node for ExpressionStatement {
-        fn token_literal(&self) -> String {
-                self.token.literal.clone()
-        }
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
 
-        fn string(&self) -> String {
-                self.expression.string()
-        }
+    fn string(&self) -> String {
+        self.expression.string()
+    }
 }
 
 pub struct LetStatement {
-        pub token: Token,
-        pub name: Identifier,
-        pub value: Expression,
+    pub token: Token,
+    pub name: Identifier,
+    pub value: BoxNode,
 }
 
 impl Node for LetStatement {
-        fn token_literal(&self) -> String {
-                self.token.literal.clone()
-        }
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
 
-        fn string(&self) -> String {
-                self.token_literal()
-                        + " "
-                        + &self.name.token.literal
-                        + " = "
-                        + &self.value.string()
-                        + ";"
-        }
+    fn string(&self) -> String {
+        self.token_literal() + " " + &self.name.token.literal + " = " + &self.value.string() + ";"
+    }
 }
 
 pub struct ReturnStatement {
-        pub token: Token,
-        pub value: Expression,
+    pub token: Token,
+    pub value: BoxNode,
 }
 impl Node for ReturnStatement {
-        fn token_literal(&self) -> String {
-                self.token.literal.clone()
-        }
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
 
-        fn string(&self) -> String {
-                self.token_literal() + " " + &self.value.string() + ";"
-        }
+    fn string(&self) -> String {
+        self.token_literal() + " " + &self.value.string() + ";"
+    }
 }
 
 pub struct Identifier {
-        pub token: Token,
-        pub value: String,
+    pub token: Token,
+    pub value: String,
 }
 
 impl Node for Identifier {
-        fn token_literal(&self) -> String {
-                self.token.literal
-        }
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
 
-        fn string(&self) -> String {
-                self.value
-        }
-}
-
-impl From<Identifier> for Expression {
-        fn from(value: Identifier) -> Self {
-                Expression(Box::new(value))
-        }
+    fn string(&self) -> String {
+        self.value.clone()
+    }
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,124 +1,112 @@
-#[cfg(test)]
-mod parser_test;
-
 use crate::{
-        ast,
-        ast::*,
-        lexer::Lexer,
-        token::{Token, TokenType},
+    ast,
+    ast::*,
+    lexer::Lexer,
+    token::{Token, TokenType},
 };
 use core::iter::Peekable;
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Deref};
 
 struct Parser {
-        lexer: Peekable<Lexer>,
+    lexer: Peekable<Lexer>,
 
-        prefix_parse_fn: HashMap<TokenType, Box<ParsePrefixFunction>>,
-        infix_parse_fn: HashMap<TokenType, Box<ParseInfixFunction>>,
+    prefix_parse_fn: HashMap<TokenType, ParseFunction>,
+    infix_parse_fn: HashMap<TokenType, ParseFunction>,
 }
 
 #[derive(Debug)]
 enum ParserError {
-        UnexpectedEOF { expected: TokenType },
-        UnexpectedToken { expected: TokenType, token: Token },
-        UnhandledError,
+    UnexpectedEOF { expected: TokenType },
+    UnexpectedToken { expected: TokenType, token: Token },
+    UnhandledError,
 }
 
-type ResultStatement = Result<Statement, ParserError>;
-type ResultExpression = Result<Statement, ParserError>;
-
-type ParsePrefixFunction = dyn Fn(&mut Parser) -> Result<Expression, ParserError>;
-type ParseInfixFunction = dyn Fn(&mut Parser) -> Result<Expression, ParserError>;
+type BoxNode = Box<dyn Node>;
+type ResultNode = Result<BoxNode, ParserError>;
+type ParseFunction = fn(&mut Parser) -> Result<BoxNode, ParserError>;
 
 impl Parser {
-        fn new(lexer: Lexer) -> Parser {
-                let lexer = lexer.peekable();
-                let prefix_parse_fn: HashMap<TokenType, Box<ParsePrefixFunction>> =
-                        HashMap::from([(
-                                TokenType::Ident,
-                                Box::new(Parser::parse_identifier_expression)
-                                        as Box<ParsePrefixFunction>,
-                        )]);
-                let infix_parse_fn = HashMap::new();
-                Parser {
-                        lexer,
-                        prefix_parse_fn,
-                        infix_parse_fn,
-                }
+    fn new(lexer: Lexer) -> Parser {
+        let lexer = lexer.peekable();
+        let prefix_parse_fn: HashMap<TokenType, ParseFunction> = HashMap::from([(
+            TokenType::Ident,
+            Parser::parse_identifier_expression as ParseFunction,
+        )]);
+        let infix_parse_fn = HashMap::new();
+        Parser {
+            lexer,
+            prefix_parse_fn,
+            infix_parse_fn,
         }
+    }
 
-        fn parse_program(self) -> Result<Program, ParserError> {
-                let statements = self.collect::<Result<Vec<Statement>, ParserError>>()?;
-                Ok(Program { statements })
-        }
+    fn parse_program(self) -> Result<Program, ParserError> {
+        let statements = self.collect::<Result<Vec<BoxNode>, ParserError>>()?;
+        Ok(Program { statements })
+    }
 
-        fn parse_let_statement(&mut self) -> ResultStatement {
-                let token = self.expect_next_token(TokenType::Let)?;
-                let name = self.parse_identifier()?;
-                self.expect_next_token(TokenType::Assign)?;
-                let value = self.parse_expression()?;
-                Ok(Statement(Box::new(LetStatement { token, name, value })))
-        }
+    fn parse_let_statement(&mut self) -> ResultNode {
+        let token = self.expect_next_token(TokenType::Let)?;
+        let name = self.parse_identifier()?;
+        self.expect_next_token(TokenType::Assign)?;
+        let value = self.parse_expression()?;
+        Ok(Box::new(LetStatement { token, name, value }))
+    }
 
-        fn parse_return_statement(&mut self) -> ResultStatement {
-                let token = self.expect_next_token(TokenType::Return)?;
-                let value = self.parse_expression()?;
-                Ok(Statement(Box::new(ReturnStatement { token, value })))
-        }
+    fn parse_return_statement(&mut self) -> ResultNode {
+        let token = self.expect_next_token(TokenType::Return)?;
+        let value = self.parse_expression()?;
+        Ok(Box::new(ReturnStatement { token, value }))
+    }
 
-        fn parse_identifier(&mut self) -> Result<ast::Identifier, ParserError> {
-                let token = self.expect_next_token(TokenType::Ident)?;
-                Ok(Identifier {
-                        value: token.literal.clone(),
-                        token,
-                })
-        }
+    fn parse_identifier(&mut self) -> Result<ast::Identifier, ParserError> {
+        let token = self.expect_next_token(TokenType::Ident)?;
+        Ok(Identifier {
+            value: token.literal.clone(),
+            token,
+        })
+    }
 
-        fn parse_expression(&mut self) -> Result<ast::Expression, ParserError> {
-                let token = self.lexer.peek().ok_or(ParserError::UnhandledError)?;
-                let prefix = self
-                        .prefix_parse_fn
-                        .get(&token.token_type)
-                        .ok_or(ParserError::UnhandledError)?
-                        .clone();
-                let left_exp = prefix(self);
-                left_exp
-        }
+    fn parse_expression(&mut self) -> ResultNode {
+        let token = self.lexer.peek().ok_or(ParserError::UnhandledError)?;
+        let prefix = self
+            .prefix_parse_fn
+            .get(&token.token_type)
+            .ok_or(ParserError::UnhandledError)?;
+        let left_exp = prefix(self);
+        left_exp
+    }
 
-        fn expect_next_token(&mut self, expected: TokenType) -> Result<Token, ParserError> {
-                match self.lexer.next() {
-                        Some(token) if token.token_type == expected => Ok(token),
-                        Some(token) => Err(ParserError::UnexpectedToken { expected, token }),
-                        None => Err(ParserError::UnexpectedEOF { expected }),
-                }
+    fn expect_next_token(&mut self, expected: TokenType) -> Result<Token, ParserError> {
+        match self.lexer.next() {
+            Some(token) if token.token_type == expected => Ok(token),
+            Some(token) => Err(ParserError::UnexpectedToken { expected, token }),
+            None => Err(ParserError::UnexpectedEOF { expected }),
         }
+    }
 
-        fn parse_identifier_expression(&mut self) -> Result<Expression, ParserError> {
-                let token = self.lexer.next().unwrap();
-                let value = token.literal.clone();
-                Ok(Expression(
-                        Box::new(Identifier { token, value }) as Box<dyn Node>
-                ))
-        }
+    fn parse_identifier_expression(&mut self) -> ResultNode {
+        let token = self.lexer.next().unwrap();
+        let value = token.literal.clone();
+        Ok(Box::new(Identifier { token, value }) as Box<dyn Node>)
+    }
 
-        fn parse_expression_statment(&mut self) -> Result<Statement, ParserError> {
-                let expression = self.parse_expression()?;
-                let token = *self.lexer.peek().unwrap().clone();
-                Ok(Statement(Box::new(ExpressionStatement {
-                        expression,
-                        token,
-                }) as Box<dyn Node>))
-        }
+    fn parse_expression_statment(&mut self) -> ResultNode {
+        let expression = self.parse_expression()?;
+        let token = self.lexer.peek().ok_or(ParserError::UnhandledError)?;
+        let token = token;
+        Ok(Box::new(ExpressionStatement { expression, token }) as Box<dyn Node>)
+    }
 }
 
 impl Iterator for Parser {
-        type Item = Result<Statement, ParserError>;
-        fn next(&mut self) -> Option<Self::Item> {
-                let token = self.lexer.peek()?;
-                match token.token_type {
-                        TokenType::Let => Some(self.parse_let_statement()),
-                        TokenType::Return => Some(self.parse_return_statement()),
-                        _ => Some(self.parse_expression_statment()),
-                }
+    type Item = ResultNode;
+    fn next(&mut self) -> Option<Self::Item> {
+        let token = self.lexer.peek()?;
+        match token.token_type {
+            TokenType::Let => Some(self.parse_let_statement()),
+            TokenType::Return => Some(self.parse_return_statement()),
+            _ => Some(self.parse_expression_statment()),
         }
+    }
 }
